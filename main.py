6221257879e3d3bafc074bf8a8bf400df06ed251
Tijana <tijana@teamlift.co>
2023-06-08 09:04:38 +0200

    accessing db

---
+++ main.py
@@ -0,0 +1,6 @@
+from neomodel import config, StructuredNode, StructuredRel, UniqueIdProperty, IntegerProperty, StringProperty, \
+    FloatProperty, \
+    RelationshipTo
+
+config.DATABASE_URL = f'neo4j://databaseNAME:PASSWORD@localhost'
+
