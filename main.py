1488045e8656cc75d6ae4ab1a88693683f6d39ec
Tijana <tijana@teamlift.co>
2023-06-27 11:06:55 +0200

    clean master code

--- main.py
+++
@@ -1,6 +0,0 @@
-from neomodel import config, StructuredNode, StructuredRel, UniqueIdProperty, IntegerProperty, StringProperty, \
-    FloatProperty, \
-    RelationshipTo
-
-#config.DATABASE_URL = f'neo4j://databaseNAME:PASSWORD@localhost'
-config.DATABASE_URL = f'neo4j://neo4j:db12345@localhost'
